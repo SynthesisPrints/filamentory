b2637b3303e3180d0c75555a6aacc9f10f459fee
Nathan Kamenar <nkamenar.it@gmail.com>
2024-02-17 10:48:48 -0500

    Reformat project

--- apps/web/db/migrate.ts
+++ apps/web/db/migrate.ts
@@ -1,32 +1,30 @@
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
-import 'dotenv/config'
-
+import 'dotenv/config';
 
 const runMigrate = async () => {
 	if (!process.env.DATABASE_URL) {
-		throw new Error('DATABASE_URL is not defined')
+		throw new Error('DATABASE_URL is not defined');
 	}
 
-	const databaseUrl = drizzle(postgres(`${process.env.DATABASE_URL}`,
-		{ ssl: 'require', max: 1 }));
+	const databaseUrl = drizzle(postgres(`${process.env.DATABASE_URL}`, { ssl: 'require', max: 1 }));
 
-	console.log('⏳ Running migrations...')
+	console.log('⏳ Running migrations...');
 
-	const start = Date.now()
+	const start = Date.now();
 
-	await migrate(databaseUrl, { migrationsFolder: './db/drizzle' })
+	await migrate(databaseUrl, { migrationsFolder: './db/drizzle' });
 
-	const end = Date.now()
+	const end = Date.now();
 
-	console.log(`✅ Migrations completed in ${end - start}ms`)
+	console.log(`✅ Migrations completed in ${end - start}ms`);
 
-	process.exit(0)
-}
+	process.exit(0);
+};
 
 runMigrate().catch((err) => {
-	console.error('❌ Migration failed')
-	console.error(err)
-	process.exit(1)
-})
+	console.error('❌ Migration failed');
+	console.error(err);
+	process.exit(1);
+});
--- apps/web/db/schema.ts
+++ apps/web/db/schema.ts
@@ -1,91 +1,97 @@
-import {
-	bigserial,
-	pgTable,
-	uniqueIndex,
-	text,
-	index,
-	timestamp,
-	jsonb,
-	numeric,
-	bigint,
-} from 'drizzle-orm/pg-core';
+import { bigserial, pgTable, uniqueIndex, text, index, timestamp, jsonb, numeric, bigint } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
-	id: bigserial("id", {mode: 'number'}).primaryKey(),
+	id: bigserial('id', { mode: 'number' }).primaryKey(),
 	username: text('username').unique(),
 	email: text('email').unique(),
 });
 
-export const locations = pgTable('locations', {
-	id: bigserial("id", {mode: 'number'}).primaryKey(),
-	name: text('name'),
-	user_id: bigint('user_id', {mode: 'number'}).references(() => users.id),
-	description: text('description'),
-	type: text('type'),
-}, (table) => ({
-	typeIndex: index('locations_type_idx').on(table.type),
-}));
+export const locations = pgTable(
+	'locations',
+	{
+		id: bigserial('id', { mode: 'number' }).primaryKey(),
+		name: text('name'),
+		user_id: bigint('user_id', { mode: 'number' }).references(() => users.id),
+		description: text('description'),
+		type: text('type'),
+	},
+	(table) => ({
+		typeIndex: index('locations_type_idx').on(table.type),
+	}),
+);
 
-export const spools = pgTable('spools', {
-	id: bigserial("id", {mode: 'number'}).primaryKey(),
-	user_id: bigint('user_id', {mode: 'number'}).references(() => users.id),
-	sku: text('sku'),
-	material: text('material'),
-	color_name: text('color_name'),
-	color: text('color'),
-	brand: text('brand'),
-	original_quantity: numeric('original_quantity', { precision: 12, scale: 2 }),
-	remaining_quantity: numeric('remaining_quantity', { precision: 12, scale: 2 }),
-	purchase_price: numeric('purchase_price', { precision: 12, scale: 2 }),
-	purchase_date: timestamp('purchase_date', { withTimezone: true }),
-	location_id: bigint('location_id', {mode: 'number'}).references(() => locations.id),
-	last_dried_date: timestamp('last_dried_date', { withTimezone: true }),
-	tags: jsonb('tags'),
-}, (table) => ({
-	skuIndex: index('spools_sku_idx').on(table.sku),
-	userIdIndex: index('spools_user_id_idx').on(table.user_id),
-	originalQuantityIndex: index('spools_original_quantity_idx').on(table.original_quantity),
-	materialIndex: index('spools_material_idx').on(table.material),
-	colorNameIndex: index('spools_color_name_idx').on(table.color_name),
-	brandIndex: index('spools_brand_idx').on(table.brand),
-}));
+export const spools = pgTable(
+	'spools',
+	{
+		id: bigserial('id', { mode: 'number' }).primaryKey(),
+		user_id: bigint('user_id', { mode: 'number' }).references(() => users.id),
+		sku: text('sku'),
+		material: text('material'),
+		color_name: text('color_name'),
+		color: text('color'),
+		brand: text('brand'),
+		original_quantity: numeric('original_quantity', { precision: 12, scale: 2 }),
+		remaining_quantity: numeric('remaining_quantity', { precision: 12, scale: 2 }),
+		purchase_price: numeric('purchase_price', { precision: 12, scale: 2 }),
+		purchase_date: timestamp('purchase_date', { withTimezone: true }),
+		location_id: bigint('location_id', { mode: 'number' }).references(() => locations.id),
+		last_dried_date: timestamp('last_dried_date', { withTimezone: true }),
+		tags: jsonb('tags'),
+	},
+	(table) => ({
+		skuIndex: index('spools_sku_idx').on(table.sku),
+		userIdIndex: index('spools_user_id_idx').on(table.user_id),
+		originalQuantityIndex: index('spools_original_quantity_idx').on(table.original_quantity),
+		materialIndex: index('spools_material_idx').on(table.material),
+		colorNameIndex: index('spools_color_name_idx').on(table.color_name),
+		brandIndex: index('spools_brand_idx').on(table.brand),
+	}),
+);
 
 export const printers = pgTable('printers', {
-	id: bigserial("id", {mode: 'number'}).primaryKey(),
+	id: bigserial('id', { mode: 'number' }).primaryKey(),
 	name: text('name'),
 	type: text('type'),
 	location_ids: jsonb('location_ids'),
 });
 
-export const user_printers = pgTable('user_printers', {
-	id: bigserial("id", {mode: 'number'}).primaryKey(),
-	user_id: bigint('user_id', {mode: 'number'}).references(() => users.id),
-	printer_id: bigint('printer_id', {mode: 'number'}).references(() => printers.id),
-	nickname: text('nickname'),
-	location_ids: jsonb('location_ids'),
-	tags: jsonb('tags'),
-}, (table) => ({
-	userNicknameIndex: uniqueIndex('user_printers_user_nickname_idx').on(table.user_id, table.nickname),
-}));
+export const user_printers = pgTable(
+	'user_printers',
+	{
+		id: bigserial('id', { mode: 'number' }).primaryKey(),
+		user_id: bigint('user_id', { mode: 'number' }).references(() => users.id),
+		printer_id: bigint('printer_id', { mode: 'number' }).references(() => printers.id),
+		nickname: text('nickname'),
+		location_ids: jsonb('location_ids'),
+		tags: jsonb('tags'),
+	},
+	(table) => ({
+		userNicknameIndex: uniqueIndex('user_printers_user_nickname_idx').on(table.user_id, table.nickname),
+	}),
+);
 
-export const prints = pgTable('prints', {
-	id: bigserial("id", {mode: 'number'}).primaryKey(),
-	name: text('name'),
-	url: text('url'),
-	user_id: bigint('user_id', {mode: 'number'}).references(() => users.id),
-	start_time: timestamp('start_time', { withTimezone: true }),
-	end_time: timestamp('end_time', { withTimezone: true }),
-	user_printer_id: bigint('user_printer_id', {mode: 'number'}).references(() => user_printers.id),
-	status: text('status'),
-	tags: jsonb('tags'),
-}, (table) => ({
-	userIdIndex: index('prints_user_id_idx').on(table.user_id),
-	userPrinterId: index('prints_user_printer_id_idx').on(table.user_printer_id),
-}));
+export const prints = pgTable(
+	'prints',
+	{
+		id: bigserial('id', { mode: 'number' }).primaryKey(),
+		name: text('name'),
+		url: text('url'),
+		user_id: bigint('user_id', { mode: 'number' }).references(() => users.id),
+		start_time: timestamp('start_time', { withTimezone: true }),
+		end_time: timestamp('end_time', { withTimezone: true }),
+		user_printer_id: bigint('user_printer_id', { mode: 'number' }).references(() => user_printers.id),
+		status: text('status'),
+		tags: jsonb('tags'),
+	},
+	(table) => ({
+		userIdIndex: index('prints_user_id_idx').on(table.user_id),
+		userPrinterId: index('prints_user_printer_id_idx').on(table.user_printer_id),
+	}),
+);
 
 export const print_spools = pgTable('print_spools', {
-	id: bigserial("id", {mode: 'number'}).primaryKey(),
-	print_id: bigint('print_id', {mode: 'number'}).references(() => prints.id),
-	spool_id: bigint('spool_id', {mode: 'number'}).references(() => spools.id),
+	id: bigserial('id', { mode: 'number' }).primaryKey(),
+	print_id: bigint('print_id', { mode: 'number' }).references(() => prints.id),
+	spool_id: bigint('spool_id', { mode: 'number' }).references(() => spools.id),
 	amount_used: numeric('amount_used', { precision: 12, scale: 2 }),
 });
