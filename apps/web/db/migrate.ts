c308fe7155cccf49fb1a56336ed5cf02a27c6db6
Nathan Kamenar <nkamenar.it@gmail.com>
2024-02-19 10:05:44 -0500

    Update drizzle config with schema

--- apps/web/db/migrate.ts
+++ apps/web/db/migrate.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
+
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const runMigrate = async () => {
