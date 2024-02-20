1f18e61c39d93a5fc166728ea78a659c62cbc853
Justin Trenary <trenaryja@gmail.com>
2024-02-20 01:03:41 -0500

    update migrate script

--- apps/web/db/migrate.ts
+++ apps/web/db/migrate.ts
@@ -3,14 +3,14 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
 const runMigrate = async () => {
+	console.log('VERCEL_ENV', process.env.VERCEL_ENV);
+	console.log('VERCEL_GIT_COMMIT_REF', process.env.VERCEL_GIT_COMMIT_REF);
+
 	if (!process.env.DATABASE_URL) {
 		throw new Error('DATABASE_URL is not defined');
 	}
 
-	console.log('VERCEL_ENV', process.env.VERCEL_ENV);
-	console.log('VERCEL_GIT_COMMIT_REF', process.env.VERCEL_GIT_COMMIT_REF);
-
-	if (process.env.VERCEL_GIT_COMMIT_REF === 'develop') {
+	if (!['main', 'staging'].includes(process.env.VERCEL_GIT_COMMIT_REF)) {
 		console.log(`✅ Skipping migrations for develop branch`);
 		process.exit(0);
 	}
