68abc26ff9b3d84c14e12066a3acd2e5b216fd56
Nathan Kamenar <nkamenar.it@gmail.com>
2024-02-24 01:06:14 -0500

    Fix issue with logo animation alignment, resolves #14

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,6 +3,21 @@ import { Config } from 'tailwindcss';
 
 const config = {
 	...sharedConfig,
+	theme: {
+		extend: {
+			keyframes: {
+				fadeInPing: {
+					'0%': { transform: 'scale(0.95)', opacity: '0' },
+					'50%': { transform: 'scale(1)', opacity: '1' },
+					'75%': { transform: 'scale(1.25)', opacity: '0' },
+					'100%': { transform: 'scale(1.5)', opacity: '0' },
+				},
+			},
+			animation: {
+				'fade-in-ping': 'fadeInPing 2s infinite',
+			},
+		},
+	},
 } satisfies Config;
 
 export default config;
