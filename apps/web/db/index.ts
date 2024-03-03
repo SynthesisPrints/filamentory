fdc4482b3f6a8a21509c69ff802e6f8b5182bfb4
Nathan Kamenar <nkamenar.it@gmail.com>
2024-03-03 00:25:14 -0500

    Fix bug from neon/drizzle update

--- apps/web/db/index.ts
+++ apps/web/db/index.ts
@@ -1,7 +1,7 @@
-import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
 // create the connection
-export const sql = neon(process.env.DATABASE_URL!);
+export const sql = neon<boolean, boolean>(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql);
